9276681d10ec98f44b383341fa6388aed7f29b5c
Julio Vasconcelos <julio_jpv@yahoo.com.br>
2025-03-25 05:28:05 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Modelos/Key.cs
@@ -0,0 +1,7 @@
+﻿namespace ScreenSound.Modelos;
+
+public static class Key
+{
+    public static readonly string 
+        ApiKey = "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";
+}
